04de37881a9e1211c0694f7b6f83d336af4079b6
Sector <foldvriadam@gmail.com>
2021-03-19 12:40:45 +0100

    Game is here

--- monke/src/monke/Main.java
+++ monke/src/monke/Main.java
@@ -1,7 +1,37 @@
 package monke;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Szisza");
+	    
+		Scanner myObj = new Scanner(System.in);
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while (true) {
+			String[] cmd = null;
+			try {
+				cmd = br.readLine().split(" ");
+			} catch (IOException e) {
+				System.out.println(e);
+			}
+			if (cmd.length == 0 || cmd[0].equals("")) {
+			}	
+			else if (cmd[0].equals("exit")) {
+				break;
+			} else if (cmd[0].equals("mine")) {
+				System.out.println("szex");
+
+			} else if (cmd[0].equals("drill")) {
+				System.out.println("Enter asteroid layer:");
+				int layer=myObj.nextInt();
+			} 
+			else {
+				System.out.println("'" + cmd[0] +"' is not recognized command");
+			}
+		}
 	}
 }
