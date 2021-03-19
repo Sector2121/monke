fc474c299941f14840d1a372aecf251a3a3f67c4
Zalán Mokos <zalanmokos@edu.bme.hu>
2021-03-19 11:47:50 +0100

    bill

--- monke/src/monke/BillOfResources.java
+++ monke/src/monke/BillOfResources.java
@@ -1,5 +1,30 @@
 package monke;
 
 public class BillOfResources {
-
+	private Resource[] billOfTpk;
+	private Resource[] billOfRobot;
+	private Resource[] billOfBase;
+	
+	public void AddResource(Resource r) {
+		
+	}
+	
+	public void RemoveResource(Resource r) {
+		
+	}
+	
+	public boolean CheckResourceTpk(Resource[] r) {
+		
+		return true;
+	}
+	
+	public boolean CheckResourceRobot(Resource[] r) {
+		
+		return true;
+	}
+	
+	public boolean CheckResourceBase(Resource[] r) {
+		
+		return true;
+	}
 }
