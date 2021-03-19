3578f87aba476de289662bc639bca15ba915a54a
Zalán Mokos <zalanmokos@edu.bme.hu>
2021-03-19 11:39:48 +0100

    sun

--- monke/src/monke/Sun.java
+++ monke/src/monke/Sun.java
@@ -1,5 +1,26 @@
 package monke;
 
 public class Sun {
-
+	private Asteroid[] asteroids;
+	private boolean sunStormNextRound;
+	
+	public void GetNewAsteroids() {
+		
+	}
+	
+	public void SunStorm() {
+		
+	}
+	
+	public void SetCritical() {
+		
+	}
+	
+	public void SetNormal() {
+		
+	}
+	
+	public void Step() {
+		
+	}
 }
