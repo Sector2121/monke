6e0d4c0eee67359294cdefc620737aaedb5078ca
Viktor Jasper Szabó <szaboviktorjasper@edu.bme.hu>
2021-03-19 13:03:55 +0100

    mine teszt

--- monke/src/monke/Main.java
+++ monke/src/monke/Main.java
@@ -24,13 +24,34 @@ public class Main {
 
 			} else if (cmd[0].equals("drill")) {
 				System.out.println("Enter asteroid layer:");
-				int layer=myObj.nextInt();
+				int layer = myObj.nextInt();
 			} else if (cmd[0].equals("mine")) {
-				
+				System.out.print("Enter asteroid layer: ");
+				int layer = Integer.parseInt(myObj.nextLine());
+				if(layer != 0) {
+					System.out.println("\tYou can't mine here!");
+				} else {
+					System.out.print("\tEnter asteroid resource: ");
+					String resource = myObj.nextLine();
+					System.out.print("\t\tEnter settler's inventory count: ");
+					int invCount = Integer.parseInt(myObj.nextLine());
+					if(invCount >= 10) {
+						System.out.println("\t\t\tYou don't have space to carry!");
+					} else {
+						switch(resource) {
+						case "iron": System.out.println("\t\t\tYou succesfully mined iron!"); break;
+						case "carbon": System.out.println("\t\t\tYou succesfully mined carbon!"); break;
+						case "uranium": System.out.println("\t\t\tYou succesfully mined uranium!"); break;
+						case "waterice": System.out.println("\t\t\tYou succesfully mined waterice!"); break;
+						default: System.out.println("\t\t\tThere is no such resource!"); break;
+						}
+					}
+					
+				}
 			} else if (cmd[0].equals("build")) {
 				
 			} else if (cmd[0].equals("place teleport")) {
-				
+				//viktor
 			} else if (cmd[0].equals("replace resource")) {
 				
 			} else if (cmd[0].equals("skip")) {
