016f112d5272d31e18907bc420bb6e757742d7b4
Noémi Zsuzsanna Péter <peternoemizsuzsanna@edu.bme.hu>
2021-03-19 11:47:01 +0100

    game

--- monke/src/monke/Game.java
+++ monke/src/monke/Game.java
@@ -1,5 +1,17 @@
 package monke;
 
 public class Game {
-
+	private Sun sun;
+	private Asteroid[] asteroids;
+	private Settler[] settlers;
+	
+	public void RemoveAsteroid(Asteroid a) {}
+	public void RemoveCreature(Creature c) {}
+	public boolean CheckSettlerLifeLines() {
+		return true;
+	}
+	public void EndGame() {}
+	public void StormActivate() {}
+	public void Start() {}
+	
 }
