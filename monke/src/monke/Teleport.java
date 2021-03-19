2c75333cac8d0485bf04a075b86fb7e10bd431c3
Noémi Zsuzsanna Péter <peternoemizsuzsanna@edu.bme.hu>
2021-03-19 11:54:55 +0100

    teleport

--- monke/src/monke/Teleport.java
+++ monke/src/monke/Teleport.java
@@ -5,6 +5,12 @@ public class Teleport implements Travel{
 	private Asteroid asteroid;
 	private Teleport pair;
 	
+	public Teleport(int i, Asteroid a, Teleport p){
+		id = i;
+		asteroid = a;
+		pair = p;
+	}
+	
 	public void SetAsteroid(Asteroid a) {}
 	public Teleport GetPair() {
 		return null;
