c9b5cf40bdc2ce3b59ed6a1e2ecc5ae0fc26af26
Ádám Pálya <palyaadam@edu.bme.hu>
2021-03-19 12:15:24 +0100

    hehe

--- monke/src/monke/Asteroid.java
+++ monke/src/monke/Asteroid.java
@@ -1,5 +1,91 @@
 package monke;
 
 public class Asteroid {
-
+	private int layers;
+	private boolean closeToSun;
+	private String isEmpty;
+	private Creature[] creatures;
+	private Resource[] resources;
+	private Travel[] neighbors;
+	
+	
+	
+	public Asteroid[] GetNeighbors(){
+		return null;
+	
+	}	
+	
+	
+	public void AddCreature(Creature c){
+		
+	}	
+	
+	public void GetWeather(String s) {
+		
+	}
+	
+	public void ReduceLayers() {
+		
+	}
+	
+	public void Remove(Creature c) {
+		
+	}
+	
+	public void Accept(Creature c) {
+		
+	}
+	
+	public void CheckEnoughResources() {
+		
+	}
+	
+	public boolean CheckNeighbor(Travel t) {
+		return false;
+		
+	}
+	
+	public int GetLayers() {
+		return layers;
+		
+	}
+	
+	public Creature[] GetCreatures() {
+		return creatures;
+		
+	}
+	
+	public Resource GetResource() {
+		return null;
+		
+	}
+	
+	public void SetResource(Resource r) {
+		
+	}
+	
+	public boolean GetCloseToSun() {
+		return false;
+		
+	}
+	
+	public void SunStorm() {
+		
+	}
+	
+	public void Explode() {
+		
+	}
+	
+	public void AddNewNeighbor(Asteroid a) {
+		
+	}
+	
+	public void RemoveNeighbor(Asteroid a) {
+		
+	}
+	
+	public void SetWeather(String w) {
+		
+	}
 }
