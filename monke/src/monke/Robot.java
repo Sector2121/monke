749649fceea49e759ef5ac16bb19c49b3bf91d1e
Zalán Mokos <zalanmokos@edu.bme.hu>
2021-03-20 10:22:23 +0100

    robot

--- monke/src/monke/Robot.java
+++ monke/src/monke/Robot.java
@@ -5,14 +5,15 @@ public class Robot extends Creature{
 	public Robot(String name, Asteroid a) {
 		this.SetName(name);
 		this.SetAsteroid(a);
+		System.out.println("Robot created");
 	}
 	
 	@Override
 	public void Die() {
-		
+		System.out.println("Robot died");
 	}
 	
 	public void Step() {
-		
+		System.out.println("Robot stepped");
 	}
 }
