2afd527fc54e2f58783b5edcf10606c15d1fb1c4
Szofficsok <79463616+Szofficsok@users.noreply.github.com>
2021-03-21 20:15:33 +0100

    Update Main.java

--- monke/src/monke/Main.java
+++ monke/src/monke/Main.java
@@ -24,7 +24,7 @@ public class Main {
 			} catch (IOException e) {
 				System.out.println(e);
 			}
-			if (cmd.length == 0 || cmd[0].equals("")) {
+			if (cmd.length == 0 || cmd[0].equals("")) { //Ha semmit nem ír be csak enter-el, vagy ha csak space-t ír és enter-el nem történik semmi
 			}	
 			else if (cmd[0].equals("exit")) { //Ezzel a paranccsal lehet kilépni a programból
 				break;
@@ -36,19 +36,19 @@ public class Main {
 				tele2.SetAsteroid(a); //Beállítjuk, az aszteroidát a teleportkapunak
 				tele.SetPair(tele2); //Párosítjuk a kapukat
 				s.Move(tele);
-			} else if (cmd[0].equals("drilluranium")) { //Ezzel a paranccsal nézzük meg mi történik uránium furásakor
+			} else if (cmd[0].equals("drilluranium")) { //Ezzel a paranccsal nézzük meg mi történik uránium fúrásakor
 				Uranium u = new Uranium();
 				Asteroid a1 = new Asteroid(u); //A tesztesetek miatt létrehozott tesztalanyok
 				s.SetAsteroid(a1);
 				a1.AddCreature(s); //Beállítjuk a szükséges kapcsolatokat
 				s.Drill();
-			} else if (cmd[0].equals("drillwaterice")) { //Ezzel a paranccsal nézzük meg mi történik vízjég furásakor
+			} else if (cmd[0].equals("drillwaterice")) { //Ezzel a paranccsal nézzük meg mi történik vízjég fúrásakor
 				Waterice w = new Waterice();
 				Asteroid a2 = new Asteroid(w); //A tesztesetek miatt létrehozott változók
 				a2.AddCreature(s); 
 				s.SetAsteroid(a2); //Beállítjuk a szükséges kapcsolatokat
 				s.Drill();
-			} else if (cmd[0].equals("drillother")) { //Ezzel a paranccsal nézzük meg mi történik a többi nyersanyag furásakor
+			} else if (cmd[0].equals("drillother")) { //Ezzel a paranccsal nézzük meg mi történik a többi nyersanyag fúrásakor
 				Carbon c = new Carbon();
 				Asteroid a3 = new Asteroid(c); //A tesztesetek miatt létrehozott változók
 				a3.AddCreature(s);
@@ -69,26 +69,26 @@ public class Main {
 				}
 			} else if (cmd[0].equals("build")) { //Ezzel a paranccsal nézzük meg mi történik robot vagy teleportkapu építéskor
 				System.out.println("Enter object to build (robot, teleport): ");
-				String string = myObj.nextLine();
-				if(string.equals("robot")) {
+				String string = myObj.nextLine(); //Beolvassuk a bekért adatot, hogy robotot vagy teleportkaput akar építeni a tesztelő, a konzolról
+				if(string.equals("robot")) { //A bekért adat alapján dől el melyik függvény hívódik meg
 					s.BuildRobot();
 				}
 				if(string.equals("teleport")) {
 					s.BuildTeleport();
 				}
-			} else if (cmd[0].equals("place_teleport")) {
+			} else if (cmd[0].equals("place_teleport")) { //Ezzel a paranccsal nézzük meg mi történik amikor teleportkaput akarunk lehelyezni
 				Settler s3 = new Settler();
-				Asteroid a5 = new Asteroid(r);
+				Asteroid a5 = new Asteroid(r); //A tesztesetek miatt létrehozott változók
 				s3.SetAsteroid(a5);
-				a5.AddCreature(s3);
+				a5.AddCreature(s3); //Beállítjuk a szükséges kapcsolatokat
 				s.PlaceTeleport(a5);
-				int teleportCount = Integer.parseInt(myObj.nextLine());
+				int teleportCount = Integer.parseInt(myObj.nextLine()); //Bekéri mennyi teleportkapu van a settlernél a tesztelőtől
 				if(teleportCount == 0) {
-					System.out.println("\t\tYou don't have a teleport to place!");
+					System.out.println("\t\tYou don't have a teleport to place!"); //Nem tud mit lerakni
 				}
-				else if(teleportCount > 0 && teleportCount < 3) {
+				else if(teleportCount > 0 && teleportCount < 3) { //Ha van nála 1 vagy 2 akkor lehelyezi az aszteroidára a kaput
 					Teleport t = new Teleport();
-					if(teleportCount == 2) {
+					if(teleportCount == 2) { //2 teleportkapu esetén párosítja őket
 						Teleport t2 = new Teleport();
 						t.SetPair(t2);
 						t2.SetPair(t);
@@ -96,30 +96,30 @@ public class Main {
 					t.SetAsteroid(a5);
 					System.out.println("\t\t\tTeleport successfully placed!"); 
 					teleportCount--;
-					System.out.println("\t\t\tRemaining teleports: " + teleportCount);
+					System.out.println("\t\t\tRemaining teleports: " + teleportCount); // Maradék kapu
 				}
 				else {
-					System.out.println("\t\tThat's not a valid number of teleports!");
+					System.out.println("\t\tThat's not a valid number of teleports!"); // Helytelen érték esetén
 				}
 			}
-			else if (cmd[0].equals("replace_resource")) {
+			else if (cmd[0].equals("replace_resource")) { //Ezzel a paranccsal nézzük meg mi történik amikor nyersanyagot szeretnénk visszahelyezni az aszteroidába
 				s.PlaceResource(r);
-			} else if (cmd[0].equals("skip")) {
+			} else if (cmd[0].equals("skip")) {  //Ezzel a paranccsal nézzük meg mi történik amikor passzol egy játékos
 				s.Skip();
-			} else if (cmd[0].equals("give_up")) {
+			} else if (cmd[0].equals("give_up")) {  //Ezzel a paranccsal nézzük meg mi történik amikor feladja egy játékos
 				System.out.println("Are you sure, you want to give up the game? Yes/No");
-				String yn = myObj.nextLine();
+				String yn = myObj.nextLine(); //Azt olvassa be, hogy a játékos biztos-e abban, hogy feladja
 				if(yn.equals("Yes")) {
 					s.GiveUp();
 				}
-			} else if (cmd[0].equals("sunstorm")) {
+			} else if (cmd[0].equals("sunstorm")) { //Ezzel a paranccsal nézzük meg mi történik amikor napvihar van
 				ArrayList<Asteroid> asteroids = new ArrayList<>();
 				asteroids.add(a);
-				Sun sun = new Sun(asteroids);
+				Sun sun = new Sun(asteroids); //A tesztesetek miatt létrehozott változók és kapcsolatok beállítása
 				sun.SunStorm();
 			}
 			else {
-				System.out.println("'" + cmd[0] +"' is not recognized command");
+				System.out.println("'" + cmd[0] +"' is not recognized command"); //Nem létező parancs esetén íródik ki
 			}
 		}
 	}
