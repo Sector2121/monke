fb7fef3bafdd7903cb4b49d9a07558b1705aa403
Szofficsok <79463616+Szofficsok@users.noreply.github.com>
2021-03-21 21:18:55 +0100

    Update Sun.java

--- monke/src/monke/Sun.java
+++ monke/src/monke/Sun.java
@@ -8,40 +8,40 @@ public class Sun {
 	private boolean sunStormNextRound;
 	
 	
-	public Sun(ArrayList<Asteroid> asteroids) {
+	public Sun(ArrayList<Asteroid> asteroids) { //Konstruktor
 		this.asteroids = asteroids;
 		this.sunStormNextRound = false;
 	}
 	
-	public void GetNewAsteroids(Asteroid asteroid) {
+	public void GetNewAsteroids(Asteroid asteroid) { //Megszerzi az új napközeli aszteroidát
 		System.out.println("Get new asteroid!");
-		asteroids.add(asteroid);
+		asteroids.add(asteroid); //Hozzáadja a megkapott aszteroidát a nap aszteroidáihoz (napközeliekhez)
 	}
 	
 	public void SunStorm() {
 		System.out.println("SunStorm was called!");
 		asteroids.get(0).GetLayers();
 		Scanner myObj = new Scanner(System.in);
-		int ertek = Integer.parseInt(myObj.nextLine());
+		int ertek = Integer.parseInt(myObj.nextLine()); //Bekéri a köpeny méretét
 		if (ertek==0) {
-			System.out.println("\tEveryone save!");
+			System.out.println("\tEveryone save!"); //Nincs köpeny, így el tudnak bújni a telepesek és robotok
 			return;
 		}
-		else if(ertek>0) {
+		else if(ertek>0) { //Van köpenye így meghívja az aszteroidára a napvihart
 			asteroids.get(0).SunStorm();
 		}
 	}
 	
-	public void SetCritical() {
+	public void SetCritical() { //Beállítja az időjárást kritikusra
 		System.out.println("Weather critical!");
 		
 	}
 	
-	public void SetNormal() {
+	public void SetNormal() { //Beállítja az időjárást normálisra
 		System.out.println("Weather normal!");
 	}
 	
-	public void Step() {
+	public void Step() { //Lép a Nap
 		System.out.println("Sun stepped!");
 	}
 }
