35061b823a743ce92cf71cb9488a117331a387b7
Szofficsok <79463616+Szofficsok@users.noreply.github.com>
2021-03-21 20:40:56 +0100

    Update Robot.java

--- monke/src/monke/Robot.java
+++ monke/src/monke/Robot.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Robot extends Creature{
 	
-	public Robot(Asteroid a) {
+	public Robot(Asteroid a) { //Konstruktor
 		System.out.println("\tRobot constructor was called");
 		a.Accept1(this);
 		SetAsteroid1(a);
@@ -13,17 +13,17 @@ public class Robot extends Creature{
 		
 	}
 	@Override
-	public void Die() {
-		asteroid.Remove(null);
+	public void Die() { //Meghal a robot
+		asteroid.Remove(null); //Meghívja, hogy eltávolítódjon a robot az aszteroidáról
 		System.out.println("\t\t\t\t\tRobot died!");
 	}
 	
-	public void Step() {
-		if(GetAsteroid().GetLayers() == 0) {
+	public void Step() { //Robot Step() fv-e
+		if(GetAsteroid().GetLayers() == 0) { //Ha nincs köpenye akkor átlép másik aszteroidára
 			ArrayList<Travel> neighbors = GetAsteroid().GetNeighbors();
 			Move(neighbors.get(0));
 		}
-		if(GetAsteroid().GetLayers() > 0) {
+		if(GetAsteroid().GetLayers() > 0) { //Ha van köpenye akkor fúr
 			Drill();
 		}
 		System.out.println("Robot stepped");
