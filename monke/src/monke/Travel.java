a61a7112bc1e89b8124f051e481d254d3fa46d3b
Viktor-boi <79787265+Viktor-boi@users.noreply.github.com>
2021-04-16 12:47:17 +0200

    travel viktor

--- monke/src/monke/Travel.java
+++ monke/src/monke/Travel.java
@@ -3,4 +3,6 @@ package monke;
 public interface Travel {
 	public void Accept(Creature c);
 	public void SetIsMoving();
+	public void RemoveNeighbor(Travel t);
+	public void AcceptTeleport(Teleport t);
 }
