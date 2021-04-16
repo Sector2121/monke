82c67e9f87fb319d26fc7b9c900fe27285fdfb42
Noémi Zsuzsanna Péter <peternoemizsuzsanna@edu.bme.hu>
2021-04-16 11:30:22 +0200

    waterice

--- monke/src/monke/Waterice.java
+++ monke/src/monke/Waterice.java
@@ -2,8 +2,7 @@ package monke;
 
 public class Waterice extends Resource{
 
-	public void CloseToSun(Asteroid a) { //Napközelség esetén a vízjég elpárolog az aszteroidából
-		System.out.println("\t\tWaterice CloseToSun was called");
-		a.SetResource(null); //null-ra állítjuk az aszteroida nyersanyagát
+	public void CloseToSun(Asteroid a) { //Napkozelseg eseten a vizjeg elparolog az aszteroidabol
+		a.SetResource(null); //null-ra allitjuk az aszteroida nyersanyagat
 	}
 }
