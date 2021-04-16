0a48f4b6cb485fa3c914c886bcc12c73596cd4a7
Noémi Zsuzsanna Péter <peternoemizsuzsanna@edu.bme.hu>
2021-04-16 11:25:39 +0200

    uranium

--- monke/src/monke/Uranium.java
+++ monke/src/monke/Uranium.java
@@ -1,9 +1,11 @@
 package monke;
 
 public class Uranium extends Resource{
-	
-	public void CloseToSun(Asteroid a) { //Napközelség esetén az urán miatt felrobban az aszteroida
+	private int exposedCount;
+	public void CloseToSun(Asteroid a) { //Napkozelseg eseten az uran miatt felrobban az aszteroida
 		System.out.println("\t\tUranium CloseToSun was called");
-		a.Explode(); //Meghívódik az aszteroidára a felrobban függvény
+		if(exposedCount == 3) {
+			a.Explode(); //Meghivodik az aszteroidara a felrobban fuggveny
+		}
 	}
 }
