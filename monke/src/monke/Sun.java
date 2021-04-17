4aea685e67b9eedbb7ce221f0adb7c2df8f9be65
Zalán Mokos <zalanmokos@edu.bme.hu>
2021-04-17 12:01:58 +0200

    komment

--- monke/src/monke/Sun.java
+++ monke/src/monke/Sun.java
@@ -7,12 +7,24 @@ public class Sun {
 	private ArrayList<Asteroid> asteroids;
 	private boolean sunStormNextRound;
 	
-	public Sun(ArrayList<Asteroid> asteroids) { //Konstruktor
+	/**
+	 * 
+	 * @param asteroids
+	 * Konstruktor.
+	 */
+	public Sun(ArrayList<Asteroid> asteroids) {
 		this.asteroids = asteroids;
 		this.sunStormNextRound = false;
 	}
 	
-	public void GetNewAsteroids() { //Megszerzi az uj napkozeli aszteroidat
+	/**
+	 * Beallitja az uj napkozeli aszteroidat.
+	 * Beallitja hot-ra.
+	 * Hozzaadja a megkapott aszteroidat a nap aszteroidaihoz (napkozeliekhez).
+	 * Visszaallitja az eddigit normal-ra.
+	 * Torli az eddigi aszteroidat a listarol.
+	 */
+	public void GetNewAsteroids() {
 		int db = 0;
 		for(Asteroid a : asteroids) {
 			db++;
@@ -22,54 +34,77 @@ public class Sun {
 			else {
 				for(Travel t : asteroids.get(i).GetNeighbors()) {
 					while(((Asteroid) t).GetWeather() != "hot") {
-						SetHot((Asteroid) t); //Beallitja hot-ra
-						asteroids.add((Asteroid) t); //Hozzaadja a megkapott aszteroidat a nap aszteroidaihoz (napkozeliekhez)
+						SetHot((Asteroid) t);
+						asteroids.add((Asteroid) t);
 						break;
 					}
 				}
-				SetNormal(asteroids.get(i));//Visszaallitja az eddigit normal-ra
-				asteroids.remove(asteroids.get(i));//Torli az aszteroidat a listarol
+				SetNormal(asteroids.get(i));
+				asteroids.remove(asteroids.get(i));
 			}
 		}
 	}
 	
+	/**
+	 * Minden napkozeli aszteroidara meghivja a napvihart.
+	 */
 	public void SunStorm() {
 		System.out.println("Sunstorm started!");
-		for(Asteroid a : asteroids) { //Minden napkozeli aszteroidara meghivja a napvihart 
+		for(Asteroid a : asteroids) {
 			a.SunStorm();
 		}
 	}
 	
-	public void SetCritical(Asteroid a) { //Beallitja a homersekletet critical-ra
+	/**
+	 * Beallitja a homersekletet critical-ra.
+	 * @param a
+	 */
+	public void SetCritical(Asteroid a) {
 		a.SetWeather("critical");
 	}
 	
-	public void SetNormal(Asteroid a) { //Beallitja a homersekletet normal-ra
+	/**
+	 * Beallitja a homersekletet normal-ra.
+	 * @param a
+	 */
+	public void SetNormal(Asteroid a) {
 		a.SetWeather("normal");
 	}
 	
-	public void SetHot(Asteroid a) { //Beallitja a homersekletet hot-ra
+	/**
+	 * Beallitja a homersekletet hot-ra.
+	 * @param a
+	 */
+	public void SetHot(Asteroid a) {
 		a.SetWeather("hot");
 	}
 	
-	public void Step() { //Lep a Nap
-		if(sunStormNextRound == true) { //Ha napvihar van, akkor mindegyik aszteroidajara meghivja a napvihart
+	/**
+	 * Lep a Nap.
+	 * Ha napvihar van, akkor mindegyik aszteroidajara meghivja a napvihart.
+	 * Visszaallitja hot-ra a napkozeli aszteroidakat.
+	 * Veletlenszeruen igazra allitja a vihar ekorejelzot.
+	 * Beallitja critical-ra a napkozeli aszteroidakat.
+	 * Frissiti a napkozeli aszteroidakat.
+	 */
+	public void Step() {
+		if(sunStormNextRound == true) {
 			SunStorm();
-			for(Asteroid a : asteroids) { //Visszaallitja hot-ra a napkozeli aszteroidakat
+			for(Asteroid a : asteroids) {
 				SetHot(a);
 			}
 		}
 		else {
 			Random rand = new Random();
 			int sz = rand.nextInt(100);
-			if(sz > 0 && sz < 10) { //Veletlenszeruen igazra allitja a vihar ekorejelzot
+			if(sz > 0 && sz < 10) {
 				sunStormNextRound = true;
-				for(Asteroid a : asteroids) { //Beallitja critical-ra a napkozeli aszteroidakat
+				for(Asteroid a : asteroids) {
 					SetCritical(a);
 				}
 			}
 			else {
-				GetNewAsteroids(); //Frissiti a napkozeli aszteroidakat
+				GetNewAsteroids();
 			}
 		}
 	}
