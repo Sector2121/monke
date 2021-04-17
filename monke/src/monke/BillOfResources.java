a323b5d44ff00bac741716a24cd81b818cb5df25
Noémi Zsuzsanna Péter <peternoemizsuzsanna@edu.bme.hu>
2021-04-17 12:43:34 +0200

    BOR javadoc

--- monke/src/monke/BillOfResources.java
+++ monke/src/monke/BillOfResources.java
@@ -10,7 +10,9 @@ public class BillOfResources {
 	private ArrayList<Resource> billOfTpk;
 	private ArrayList<Resource> billOfRobot;
 	private ArrayList<Resource> billOfBase;
-	
+	/**
+	 * Konstruktor.
+	 */
 	public BillOfResources() {
 		this.billOfTpk = new ArrayList<Resource>();
 		this.billOfRobot = new ArrayList<Resource>();
@@ -33,19 +35,33 @@ public class BillOfResources {
 			this.billOfBase.add(u);
 		}
 	}
-	
+	/**
+	 * Visszaadja a teleport receptjet.
+	 * @return Visszaadja a billOfTpk
+	 */
 	public ArrayList<Resource> GetBillOfTpk() {
 		return this.billOfTpk;
 	}
-	
+	/**
+	 * Visszaadja a robot receptjet.
+	 * @return Visszaadja a billOfRobot
+	 */
 	public ArrayList<Resource> GetBillOfRobot() {
 		return this.billOfRobot;
 	}
-	
+	/**
+	 * Visszaadja a bazis receptjet.
+	 * @return Visszaadja a billOfBase
+	 */
 	public ArrayList<Resource> GetBillOfBase() {
 		return this.billOfBase;
 	}
-	
+	/**
+	 * A sztringkent megkapott objektumnak a receptjeben megnézi, hogy van-e hozza eleg nyersanyag es ettol fuggoen ad vissza booleant.
+	 * @param resources
+	 * @param valami
+	 * @return Ha van nala, akkor true-t ad vissza, ha nincs, akkor false-t.
+	 */
 	public boolean CheckResource(ArrayList<Resource> resources, String valami) {
 		List<Resource> check = new ArrayList<Resource>();
 		if(valami == "Robot") {
