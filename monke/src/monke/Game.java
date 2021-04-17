34c9edf8a10a23faaa7d5a6742cc6ce159703158
Noémi Zsuzsanna Péter <peternoemizsuzsanna@edu.bme.hu>
2021-04-17 12:06:56 +0200

    game komment

--- monke/src/monke/Game.java
+++ monke/src/monke/Game.java
@@ -6,46 +6,69 @@ public class Game {
 	private Sun sun;
 	private ArrayList<Asteroid> asteroids;
 	private ArrayList<Settler> settlers;
-	
-	public Game(){ //Konstruktor
+	/**
+	 * Konstruktor.
+	 */
+	public Game(){
 		asteroids = new ArrayList<>();
 		sun = new Sun(asteroids);
 		settlers = new ArrayList<>();
 	}
-	
-	public ArrayList<Settler> GetSettlers() { //Visszaadja a jatek settlereinek listajat
+	/**
+	 * Visszaadja a jatek settlereinek listajat.
+	 * @return Jatek settler listaja.
+	 */
+	public ArrayList<Settler> GetSettlers() {
 		return this.settlers;
 	}
-	
-	public ArrayList<Asteroid> GetAsteroid() { //Visszaadja a jatek aszteroidainak listajat
+	/**
+	 * Visszaadja a jatek aszteroidainak listajat.
+	 * @return Jatek aszteroida listaja.
+	 */
+	public ArrayList<Asteroid> GetAsteroid() {
 		return this.asteroids;
 	}
-	
-	public void RemoveAsteroid(Asteroid a) { //Eltavolitja a kapott aszteroidat a jatekbol
+	/**
+	 * Eltavolitja a kapott aszteroidat a jatekbol.
+	 * @param a
+	 */
+	public void RemoveAsteroid(Asteroid a) {
 		asteroids.remove(a);
 	}
-	
-	public void RemoveSettler(Settler s) { //Eltavolitja a kapott settler-t a jatekbol
+	/**
+	 * Eltavolitja a kapott settler-t a jatekbol.
+	 * @param s
+	 */
+	public void RemoveSettler(Settler s) {
 		settlers.remove(s);
 	}
-	
-	public boolean CheckSettlerLifeLines() { //Ellenorzi, hogy el-e meg settler
-		if(settlers.isEmpty() == true) {//Ha nem el tobb settler, akkor false-t ad
+	/**
+	 * Ellenorzi, hogy el-e meg settler.
+	 * @return Ha nem el tobb settler, akkor false-t ad, ha meg el valaki akkor true.
+	 */
+	public boolean CheckSettlerLifeLines() {
+		if(settlers.isEmpty() == true) {
 			return false;
 		}
 		else {
-			return true; //Ha meg el valaki akkor true
+			return true;
 		}
 	}
-	
-	public void EndGame() { //Jatek befejezese
-		if(CheckSettlerLifeLines() == true) { //Meghivodott ez a fuggveny es meg elnek telepesek, igy nyertek
+	/**
+	 * Jatek befejezese.
+	 * Meghivodott ez a fuggveny es meg elnek telepesek, igy nyertek.
+	 * Meghivodott ez a fuggveny es nem elnek mar telepesek, igy vesztettek.
+	 */
+	public void EndGame() {
+		if(CheckSettlerLifeLines() == true) {
 			System.out.println("A jateknak vege, a telepesek nyertek!");
 		}
-		else {//Meghivodott ez a fuggveny es nem elnek mar telepesek, igy vesztettek
+		else {
 			System.out.println("A jateknak vege, a telepesek veszitettek!");
 		}
 	}
-	
-	public void Start() {} //Jatek elinditasa
+	/**
+	 * Jatek elinditasa.
+	 */
+	public void Start() {}
 }
