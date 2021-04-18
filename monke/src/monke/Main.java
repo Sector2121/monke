a4cda5abe669af1e39b1e035914a72d65a89865e
Viktor Jasper Szabó <szaboviktorjasper@edu.bme.hu>
2021-04-18 17:52:29 +0200

    a

--- monke/src/monke/Main.java
+++ monke/src/monke/Main.java
@@ -112,11 +112,11 @@ public class Main {
 				}
 			} else if (cmd[0].equals("Build")) {
 				for (Settler s : game.GetSettlers()) {
-					if(s.GetName().equals(cmd[2])) {
-						if(cmd[1].equals("robot")) {
+					if(s.GetName().equals(cmd[1])) {
+						if(cmd[2].equals("robot")) {
 							s.BuildRobot(cmd[3]);
 						}
-						else if (cmd[1].equals("teleport")) {
+						else if (cmd[2].equals("teleport")) {
 							s.BuildTeleport();
 						}
 						else {
