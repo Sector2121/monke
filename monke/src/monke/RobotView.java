6f06719e98ece8f10b862ed8cc41d2c83e8fdbb7
Zalán Mokos <zalanmokos@edu.bme.hu>
2021-05-10 10:15:32 +0200

    robot komment

--- monke/src/monke/RobotView.java
+++ monke/src/monke/RobotView.java
@@ -9,6 +9,12 @@ public class RobotView implements Drawable{
 	private View v;
 	private JLabel label;
 	
+	/**
+	 * Konstruktor
+	 * @param x
+	 * @param y
+	 * @param v
+	 */
 	public RobotView(int x, int y, View v) {
 		this.x=x;
 		this.y=y;
@@ -18,18 +24,28 @@ public class RobotView implements Drawable{
 		label.setIcon(new ImageIcon("bb8.png"));
 	}
 	
+	/**
+	 * Beállítja a robot koordinátáit a kapott aszeroidához
+	 * @param a
+	 */
 	@Override
 	public void SetCoord(Asteroid a) {
 		x = a.GetView().GetX();
 		y = a.GetView().GetY();
 	}
 
+	/**
+	 * Kirajzolja a robotot
+	 */
 	@Override
 	public void Draw() {
 		label.setBounds(x + 20, y - 25, 30, 30);
 		v.add(label);
 	}
 	
+	/**
+	 * Halal utan atallitja a robot kepet uresre
+	 */
 	@Override
 	public void SetTomb() {
 		label.setIcon(new ImageIcon("semmi.png"));
