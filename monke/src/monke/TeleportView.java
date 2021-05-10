1721f0833d2decd34fa109728bdd9602d36cb4a4
Noémi Zsuzsanna Péter <peternoemizsuzsanna@edu.bme.hu>
2021-05-10 10:24:26 +0200

    miau

--- monke/src/monke/TeleportView.java
+++ monke/src/monke/TeleportView.java
@@ -9,7 +9,12 @@ public class TeleportView implements Drawable {
 	private int y;
 	private View v;
 	private JLabel label;
-	
+	/**
+	 * Konstruktor.
+	 * @param x
+	 * @param y
+	 * @param v
+	 */
 	public TeleportView(int x, int y, View v) {
 		this.x=x;
 		this.y=y;
@@ -17,19 +22,25 @@ public class TeleportView implements Drawable {
 		label = new JLabel();
 		label.setIcon(new ImageIcon("teleport.png"));
 	}
-	
+	/**
+	 * Kirajzolja a helyere az aszteroidahoz kepest.
+	 */
 	@Override
 	public void Draw() {
 		label.setBounds(x + 10, y + 40, 30, 30);
 		v.add(label);
 	}
-
+	/**
+	 * Beallitja a helyet a parameterkent kapott aszteroidhoz.
+	 */
 	@Override
 	public void SetCoord(Asteroid a) {
 		x = a.GetView().GetX();
 		y = a.GetView().GetY();
 	}
-	
+	/**
+	 * Ures fv. ami megvalositja a drawable interface fv-et
+	 */
 	@Override
 	public void SetTomb() {}
 }
